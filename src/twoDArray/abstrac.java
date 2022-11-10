b1d8631b096d97dbf65f323b12a77b7c57a43337
sanmithap19 <sanmithap19@gmail.com>
2022-11-10 23:15:47 +0530

    new

---
+++ src/twoDArray/abstrac.java
@@ -0,0 +1,41 @@
+package twoDArray;
+
+
+
+
+public class abstrac {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Horse h=new Horse();
+        h.eat();
+        h.walk();
+
+//Chicken c= new Chicken();
+//c.eat();
+//c.walk();
+
+    }
+
+}
+
+abstract class animals{
+    void eat() {
+        System.out.println("animal eats");
+
+    }
+    abstract void walk();
+
+
+
+}
+class Horse extends animals{
+    void walk() {
+        System.out.println("walk on 4 legs");
+    }
+    class Chicken extends animals{
+        void walk() {
+            System.out.println("walk on 2 legs");
+        }
+    }
+}
